67c8fa7e86ee26e80c7ac03490d81501d4eb2b58
Grace Park <40474035+parkg748@users.noreply.github.com>
2018-09-05 18:15:03 -0700

    Add files via upload

---
+++ Associations_Exercise/app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :Enrollment
+    
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
+  
+  has_many :teaching_courses,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :Course
+end
